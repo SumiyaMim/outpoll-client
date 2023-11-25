e1b34ac5a07df6e46674c497ce762b9f1f12a52d
SumiyaMim <sumiyaislam1093@gmail.com>
2023-11-25 18:11:25 +0600

    add how it works section

--- src/components/Home/Works.jsx
+++ src/components/Home/Works.jsx
@@ -8,7 +8,7 @@ const Works = () => {
             <SectionTitle heading="How It Works" subHeading="Empower decisions through interactive surveys, engaging audiences, and deriving insights on our secure platform."></SectionTitle>
             <div className="bg-white p-6 md:p-8 lg:p-10 rounded-md">
                 <div
-                    className="relative after:absolute after:inset-x-0 after:top-[20px] md:after:top-1/2 after:block after:h-[1px] md:after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-200"
+                    className="relative after:absolute after:inset-x-0 after:top-[20px] md:after:top-1/2 after:block after:h-[1px] md:after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-200 after:w-[220px] after:mx-auto md:after:w-full"
                 >
                     <ol
                     className="relative z-10 flex justify-between text-center items-center text-[10px] lg:text-sm font-medium text-gray-600"
